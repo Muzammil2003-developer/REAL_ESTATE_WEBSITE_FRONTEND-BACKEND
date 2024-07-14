c527da549f2e59c2b2adc6b83a4668f3330f2db8
Muzammil Ijaz <muzammilijaz2003@gmail.com>
2024-07-14 22:37:14 +0500

    Add files via upload

---
+++ realEstateWebsite/components/connect.php
@@ -0,0 +1,26 @@
+<?php
+
+$db_name = 'mysql:host=localhost;dbname=home_db';
+$db_user_name = 'root';
+$db_user_pass = '';
+
+try {
+    $conn = new PDO($db_name, $db_user_name, $db_user_pass);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+function create_unique_id(){
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < 20; $i++) {
+        $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+?>
